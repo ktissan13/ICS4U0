10223cc5275c9952a0de5b92b21813d647e90e5b
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2019-11-12 12:01:03 -0500

    Update wordstat.py

--- File IO/wordstat.py
+++ File IO/wordstat.py
@@ -5,21 +5,26 @@
 
 order = []
 
-with open('File IO/wordstats.txt') as file:
+with open('wordstats.txt') as file:
     content = list(file)
     print(content[0])
-    for line in range(1, len(content)):
-        for nt_line in range(1, len(content)):
-            if not line == nt_line:
-                if content[line][0] == content[nt_line][0]:
-                    if ord(content[line][1]) > ord(content[nt_line][1]):
-                        highest = line
+    line = 1
+    nt_line = line + 1
+    while len(order) != len(content)-2:
+        if line not in order and nt_line not in order:
+            if content[line][0] == content[nt_line][0]:
+                if ord(content[line][1]) > ord(content[nt_line][1]):
+                    highest = line
 
+            else:
+                if ord(content[line][0]) > ord(content[nt_line][0]):
+                    highest = nt_line
                 else:
-                    if ord(content[line][0]) > ord(content[line][0]):
-                        highest = line
-                    else:
-                        highest = nt_line
-                order.append(highest)
-
-print(order)
+                    highest = line
+        order.append(highest)
+        nt_line += 1
+    for line in range(1,len(content)):
+        if line not in order:
+            order.append(line)
+    for line in order:
+        print(content[line])
