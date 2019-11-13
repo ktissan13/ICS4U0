3bda5a014bb2ea21489b1e2926defda630b50d92
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2019-11-13 12:24:04 -0500

    Create TestProcessor.py

---
+++ File IO/testprocessor/TestProcessor.py
@@ -0,0 +1,27 @@
+# TestProcessor
+# Tissan Kugathas
+# ICS4U0
+# November 12 2019
+
+name = []
+answers = []
+correct = []
+
+with open('File IO/testprocessor/testprocessor.txt') as file:
+    content = list(file)
+    answer = content[0]
+    for line in range(1,len(content)):
+        if line/2 != line//2:
+            name.append(content[line].replace('\n',''))
+        else:
+            answers.append(content[line].replace('\n',''))
+
+    for ans in answers:
+        right = 0
+        for char in range(len(answer)-1):
+            if ans[char] == answer[char]:
+                right += 1
+        correct.append(right)
+
+    for current in range(len(name)):
+        print('%-12s%-5s'%(name[current],((str(round((correct[current]/(len(answer)-1))*100))+'%'))))
