0262a42288e6ab1043129bb8ceb243e328e263b6
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2019-11-16 16:41:51 -0500

    Create mergefiles.py

---
+++ File IO/Merge Files/mergefiles.py
@@ -0,0 +1,11 @@
+# Merge Files
+# Tissan Kugathas
+# ICS4U0
+# November 14 2019
+
+with open('File IO/Merge Files/temp.tmp','r+') as final:
+    with open('File IO/Merge Files/file1.txt','r') as file1:
+        list1 = list(file1)
+        with open('File IO/Merge Files/file2.txt','r') as file2:
+            list2 = list(file2)
+            fml = death 
