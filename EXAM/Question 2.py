bb7f58f9ff663f19dab717bd1b6023437fbce0af
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2020-01-19 18:56:42 -0500

    Update Question 2.py

--- EXAM/Question 2.py
+++ EXAM/Question 2.py
@@ -5,6 +5,7 @@
 
 # inputs list to keep track of the weight of the carts
 inputs = []
+valid = []
 
 # compiles the file used to get the input
 input_file = open('input.dat', 'r')
@@ -12,8 +13,15 @@ temp = list(input_file)
 for line in temp:
     inputs.append(line.replace('\n', ''))
 # sets the w as the max weigtt of the bridge and n as the number of carts
+
+# for i in range(2):
+    # inputs.append(input())
+
 w = int(inputs[0])
 n = int(inputs[1])
+
+# for i in range(n):
+# inputs.append(input())
 
 
 # checks if the varaibles are valid
@@ -26,19 +34,23 @@ if 1 <= w <= 100000:
         # while loop to go through all the carts and to see if they can go through the bridge
         while status:
             current_weight = 0
-            for cart in range(last_cart, last_cart+4):
+            for cart in range(last_cart, last_cart+5):
                 if cart <= n and int(inputs[cart]) <= w:
                     current_weight += int(inputs[cart])
-                if cart > n or int(inputs[cart]) > w:
+                if int(inputs[cart]) > w:
                     status = False
                     break
-
-            last_cart += 1
 
             if current_weight > 100:
                 status = False
             elif last_cart-2 >= n:
                 status = False
+            else:
+                for cart in range(last_cart, last_cart+5):
+                    print(cart)
+                    if cart not in valid:
+                        valid.append(cart)
 
+            last_cart += 1
 # print the number of carts that can go through
-print(last_cart-2)
+print(len(valid))
